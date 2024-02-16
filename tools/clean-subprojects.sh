c3c7602f1563d3c3567dfb87ae74a24e44f31ce9
Gerion Entrup <entrup@sra.uni-hannover.de>
2024-02-16 15:06:53 +0100

    clean-subprojects: ask before deleting

--- tools/clean-subprojects.sh
+++ tools/clean-subprojects.sh
@@ -1,7 +1,24 @@
 #!/bin/zsh
 
+setopt extended_glob
+
 script_dir=${0:a:h}
 
 cd "$script_dir"/../subprojects
-rm -rf *(/)
+for dir in ^packagecache/; do
+	cd $dir
+	if [[ -d '.git' ]]; then
+		echo "--------- Git status of $dir ----------"
+		git status
+	else
+		echo "--------- $dir (not a git dir) ----------"
+	fi
+	cd -
+done
+echo "--------------------------------"
+echo "WARNING. This script removes all subprojects and wrap-redirect independent of their state."
+echo "Commit all your changes before. I have listed them for you."
+read -s -k '?Press any key to continue...'
+
+rm -rf ^packagecache/
 for file in *.wrap; do if grep "wrap-redirect" "$file"; then echo $file; rm $file; fi; done
