711ba459f33f30a557f417de705895584b95caa9
Andreas Kässens <kaessens@sra.uni-hannover.de>
2024-04-22 13:57:36 +0200

    Docker: add debian12 based image with all parrot dependencies

---
+++ Docker/run-docker.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: 2022 Björn Fiedler <fiedler@sra.uni-hannover.de>
+# SPDX-FileCopyrightText: 2024 Andreas Kässens <kaessens@sra.uni-hannover.de>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+export uid=$(id -u)
+export gid=$(id -g)
+
+# create docker container: docker build -t parrot .
+
+docker run --rm -it \
+	--user $uid:$gid  \
+	--env=USER="$USER" \
+	--workdir "$(pwd)" \
+	--volume "/etc/group:/etc/group:ro"\
+	--volume "/etc/passwd:/etc/passwd:ro"\
+	--volume "/etc/shadow:/etc/shadow:ro"\
+	--volume "$HOME:$HOME:rw" \
+	--name parrot-dev \
+	parrot
