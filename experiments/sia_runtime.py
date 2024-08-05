68c708d206652547f22a678fd52a1a56cbfb0d89
Gerion Entrup <entrup@sra.uni-hannover.de>
2024-08-05 15:08:07 +0200

    sia_runtime: execute sias in parallel

--- experiments/sia_runtime.py
+++ experiments/sia_runtime.py
@@ -3,13 +3,16 @@
 import argparse
 import copy
 import json
+import multiprocessing
 import subprocess
+import sys
 import time
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from pathlib import Path
 
 from versuchung.experiment import Experiment
-from versuchung.types import String, List
+from versuchung.types import String, List, Integer
 from versuchung.files import Directory
 from versuchung.tex import LuaTable
 
@@ -17,6 +20,8 @@ ARA_CONFIG = {
     "steps": ["SIA"],
     "SIA": {"traversal_mode": "TBD"},
 }
+
+job_count = None
 
 
 class TimeMeasure:
@@ -34,6 +39,7 @@ class SiaRuntimeExperiment(Experiment):
     inputs = {
         "result_dir": Directory(default_filename="sia_measurements_results"),
         "ara": String(default_value="ara"),
+        "iterations": Integer(default_value=10),
         "applications": List(String),
         "modes": List(
             String,
@@ -45,10 +51,14 @@ class SiaRuntimeExperiment(Experiment):
         ),
     }
     outputs = {
-        "results": LuaTable(filename="sia-runtime.lua", experiment_name="experiments.sia_runtime"),
+        "results": LuaTable(
+            filename="sia-runtime.lua", experiment_name="experiments.sia_runtime"
+        ),
     }
 
-    def __init__(self, *args, python=None, run_dir=None, title=None, cwd=None, **kwargs):
+    def __init__(
+        self, *args, python=None, run_dir=None, title=None, cwd=None, **kwargs
+    ):
         Experiment.__init__(self, *args, **kwargs)
         self.python = python
         self.run_dir = Path(run_dir)
@@ -56,9 +66,36 @@ class SiaRuntimeExperiment(Experiment):
         if title:
             self.title = title
 
-    def run_sia(self, application_info, mode):
+    @staticmethod
+    def run_sia(app_name, cur_dir, cmd, mode, idx):
+        cur_dir = cur_dir / str(idx)
+        cur_dir.mkdir(exist_ok=True)
         timer = TimeMeasure()
+        prefix = ["--dump-prefix", str(cur_dir.absolute()) + "/{step_name}.{uuid}."]
+        cmd += prefix
+        try:
+            with timer:
+                subprocess.run(cmd, check=True)
+        except subprocess.CalledProcessError as e:
+            print("Execution failed")
+            print(" ".join([f"'{x}'" for x in cmd]))
+            raise e
 
+        sia_time = 0
+        with open(cur_dir / "ARA.-.runtime_stats.json") as stats_file:
+            for step in json.load(stats_file):
+                if step[0] == "SIA":
+                    sia_time += step[2]
+
+        return {
+            "app_name": app_name,
+            "mode": str(mode),
+            "index": idx,
+            "sia_time": sia_time,
+            "ara_time": timer.get_time(),
+        }
+
+    def prepare_sia(self, pool, executor, application_info, mode):
         app_info = json.loads(application_info.value)
 
         print("Loading:", app_info)
@@ -71,11 +108,11 @@ class SiaRuntimeExperiment(Experiment):
         cur_config = copy.copy(ARA_CONFIG)
         cur_config["SIA"]["traversal_mode"] = mode.value
 
-        cur_dir = (self.run_dir / f"{app_name}.{mode}")
+        cur_dir = self.run_dir / f"{app_name}.{mode}"
         cur_dir.mkdir(exist_ok=True)
 
         step_data = cur_dir / f"step_data_sia_{app_name}_{mode}.json"
-        with open(step_data, 'w') as c_f:
+        with open(step_data, "w") as c_f:
             c_f.write(json.dumps(cur_config, indent=2))
 
         extra_config = []
@@ -83,52 +120,64 @@ class SiaRuntimeExperiment(Experiment):
             extra_config = ["--step-settings", str(app_config.absolute())]
 
         cmd = [
-            self.python, self.inputs.ara.value, app_ll,
+            self.python,
+            self.inputs.ara.value,
+            app_ll,
             "--runtime-stats",
-            "--dump-prefix", str(cur_dir.absolute()) + "/{step_name}.{uuid}.",
             "--runtime-stats-file", "dump",
             "--runtime-stats-format", "json",
-            "--os", app_os,
+            "--os",
+            app_os,
         ]
         cmd.extend(extra_config)
         cmd.extend(["--step-settings", str(step_data.absolute())])
-        print("Executing", ' '.join([f"'{x}'" for x in cmd]))
-        for i in range(1):
-            try:
-                with timer:
-                    subprocess.run(cmd, check=True)
-                with open(cur_dir / 'ARA.-.runtime_stats.json') as stats_file:
-                    sia_time = 0
-                    for step in json.load(stats_file):
-                        if step[0] == "SIA":
-                            sia_time += step[2]
-                    self.outputs.results[app_name][str(mode)]['sia_runtime'][i] = sia_time
-
-                self.outputs.results[app_name][str(mode)]['ara_runtime'][i] = timer.get_time()
-            except subprocess.CalledProcessError as e:
-                print("Execution failed")
-                print(' '.join([f"'{x}'" for x in cmd]))
-                raise e
+        print("Executing", " ".join([f"'{x}'" for x in cmd]))
+        for i in range(self.iterations.value):
+            pool.add(
+                executor.submit(
+                    SiaRuntimeExperiment.run_sia, app_name, cur_dir, cmd, mode, i
+                )
+            )
 
     def run(self):
-        self.outputs.results['metadata']['cmdline'] = 'meson compile ' + self.title
-        for application_info in self.inputs.applications:
-            for mode in self.inputs.modes:
-                self.run_sia(application_info, mode)
+        self.outputs.results["metadata"]["cmdline"] = "meson compile " + self.title
+        with ThreadPoolExecutor(max_workers=job_count) as executor:
+            pool = set()
+            for application_info in self.inputs.applications:
+                for mode in self.inputs.modes:
+                    self.prepare_sia(pool, executor, application_info, mode)
+
+            for future in as_completed(pool):
+                res = future.result()
+                self.outputs.results[res["app_name"]][res["mode"]]["sia_runtime"][
+                    res["index"]
+                ] = res["sia_time"]
+                self.outputs.results[res["app_name"]][res["mode"]]["ara_runtime"][
+                    res["index"]
+                ] = res["ara_time"]
 
 
 if __name__ == "__main__":
-    import sys
-    print(sys.argv)
+    print("Executed with:", sys.argv)
     parser = argparse.ArgumentParser()
-    parser.add_argument("--work-dir", help="Working directory (must be able to store files there).")
+    parser.add_argument(
+        "--work-dir", help="Working directory (must be able to store files there)."
+    )
     parser.add_argument("--title", help="Experiment title")
     parser.add_argument("--python", help="Path of the python interpreter.")
+    parser.add_argument(
+        "--jobs",
+        default=multiprocessing.cpu_count(),
+        help="Maximal number of parallel SIA executions.",
+    )
     args, unknown = parser.parse_known_args()
+    job_count = args.jobs
 
     cwd = Path.cwd()
 
-    experiment = SiaRuntimeExperiment(python=args.python, run_dir=args.work_dir, title=args.title, cwd=cwd)
+    experiment = SiaRuntimeExperiment(
+        python=args.python, run_dir=args.work_dir, title=args.title, cwd=cwd
+    )
     dirname = experiment(unknown)
 
     print(dirname)
