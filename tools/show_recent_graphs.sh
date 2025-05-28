7218f94c95fdf170531280732008ecc13035856d
Mareike Burg <mareike.burg@web.de>
2025-05-28 13:52:04 +0200

    Add automatic copy of dot files into ma folder

--- tools/show_recent_graphs.sh
+++ tools/show_recent_graphs.sh
@@ -5,15 +5,17 @@
 
 # Function to display usage information
 usage() {
-    echo "Usage: $0 [-svf] [-mstg] [-abbs] [-bbs] [-inst] [-call] [-2]"
+    echo "Usage: $0 [-svf] [-mstg] [-abbs] [-bbs] [-inst] [-call] [-2] [-3] [-cp <directory>]"
 }
 
 path="../build/dumps/"
 graphs=()
 show_second=false
 show_third=false
+copy_path="../../ma_mareike_burg/fig"   # hardcoded!
 # Check arguments
-for arg in "$@"; do
+while [[ $# -gt 0 ]]; do
+    arg="$1"
     case $arg in
         -svf)
             graphs+=("SVFAnalyses*.svfg.dot" "SVFAnalyses*.svf-callgraph.dot" "SVFAnalyses*.svf-icfg.dot")
@@ -54,6 +56,17 @@ for arg in "$@"; do
 			show_third=true
             shift
             ;;
+        -cp)
+            shift # Get to next param
+            if [[ $# -eq 0 ]]; then
+                echo "No directory for -cp"
+                usage
+                exit 1
+            fi
+            copy_name="$1"
+            full_copy_path="${copy_path}/${copy_name}/"
+            shift
+            ;;
         -h|--help)
             usage
             exit 0
@@ -66,10 +79,19 @@ for arg in "$@"; do
     esac
 done
 
+
+echo "Current path is: $(pwd)"
+if [[ -n "${full_copy_path}" ]]; then
+    echo "Copy to $full_copy_path"
+    mkdir -p "$full_copy_path"
+fi
+
+echo ""
+
 count=0
 for i in "${!graphs[@]}"; do
 	graph=${graphs[$i]}
-	echo "${path}${graph}"
+	echo "Matching: ${path}${graph}"
 	read -d '' -r -a recent_files < <(ls -t ${path}${graph} 2>/dev/null)
 
 	if [ ${#recent_files[@]} -gt 0 ]; then
@@ -78,11 +100,29 @@ for i in "${!graphs[@]}"; do
 		echo "1: $most_recent"
 		count=$((count + 1))
 
+        if [[ -n "${full_copy_path}" ]]; then
+            modified_name_a=$(echo "$most_recent" | cut -d '/' -f4 | cut -d '.' -f1)
+            modified_name_b=$(echo "$most_recent" | cut -d '/' -f4 | cut -d '.' -f3,4,5,6,7,8)
+            trimmed_name=$(echo "$copy_name" | tr -d "/")
+            new_name="${modified_name_a}.${trimmed_name}.${modified_name_b}"
+            cp "$most_recent" "${copy_path}/${copy_name}/${new_name}"
+            echo "Copied to ${new_name}"
+        fi
+
 		if $show_second && [ ${#recent_files[@]} -gt 1 ]; then
 			second_most_recent=${recent_files[1]}
 			xdot "$second_most_recent" > /dev/null 2>&1 &
 			echo "2: $second_most_recent"
 			count=$((count + 1))
+
+            if [[ -n "${full_copy_path}" ]]; then
+                modified_name_a=$(echo "$second_most_recent" | cut -d '/' -f4 | cut -d '.' -f1)
+                modified_name_b=$(echo "$second_most_recent" | cut -d '/' -f4 | cut -d '.' -f3,4,5,6,7,8)
+                trimmed_name=$(echo "$copy_name" | tr -d "/")
+                new_name="${modified_name_a}.${trimmed_name}.${modified_name_b}"
+                cp "$second_most_recent" "${copy_path}/${copy_name}/${new_name}"
+                echo "Copied to ${new_name}"
+            fi
 		fi
 
 		if $show_third && [ ${#recent_files[@]} -gt 2 ]; then
@@ -90,11 +130,20 @@ for i in "${!graphs[@]}"; do
 			xdot "$third_most_recent" > /dev/null 2>&1 &
 			echo "3: $third_most_recent"
 			count=$((count + 1))
+
+            if [[ -n "${full_copy_path}" ]]; then
+                modified_name_a=$(echo "$third_most_recent" | cut -d '/' -f4 | cut -d '.' -f1)
+                modified_name_b=$(echo "$third_most_recent" | cut -d '/' -f4 | cut -d '.' -f3,4,5,6,7,8)
+                trimmed_name=$(echo "$copy_name" | tr -d "/")
+                new_name="${modified_name_a}.${trimmed_name}.${modified_name_b}"
+                cp "$third_most_recent" "${copy_path}/${copy_name}/${new_name}"
+                echo "Copied to ${new_name}"
+            fi
 		fi
 	else
 		echo "No file found matching pattern $graph"
 	fi
 done
 
-echo "Current dir is: $(pwd)"
+echo ""
 echo "Showed ${count} graphs."
